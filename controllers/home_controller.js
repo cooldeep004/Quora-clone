7be9d3a5802499532941985c494ca1da76c3724a
cooldeep004 <kuldeepsinghbvpcoep@gmail.com>
2020-10-18 03:34:24 +0530

    add post and display

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,6 +1,11 @@
+const User=require('../models/user');
+const Post=require('../models/post');
 module.exports.home=function(req,res){
     //console.log(req.cookies);
-    return res.render('home',{
-        title:'Home'
+    Post.find({},function(err,posts){
+        return res.render('home',{
+            title:'Home',
+            posts:posts
+        });
     });
 }
